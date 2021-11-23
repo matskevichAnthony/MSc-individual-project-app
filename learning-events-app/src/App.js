0e87db47cc6e18aff93c8621f65dbbb2094859e2
matskevichAnthony <91896307+matskevichAnthony@users.noreply.github.com>
2021-11-23 10:23:57 +0000

    Old navigation deleted

--- learning-events-app/src/App.js
+++ learning-events-app/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { useSelector, useDispatch } from 'react-redux';
-import Nav from "./components/Nav";
 import Navigation from './components/Navigation'
 import Home from './pages/Home'
 import { Router, Switch, Route, useLocation, useParams, useRouteMatch } from 'react-router-dom';
